8afb405d5f5e4503c3665a930318203e20e0d20c
Eli Marrone <elmarron@microsoft.com>
2018-07-02 16:26:14 -0700

    first commit

---
+++ App2/ErrorDisplay.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
+
+namespace DisplayInkApp
+{
+    class ErrorDisplay
+    {
+
+        public static async void ShowErrorMessage(string err)
+        {
+            // Create the message dialog and set its content
+            var messageDialog = new MessageDialog("An error occured: " + err);
+
+            // Add commands and set their callbacks; both buttons use the same callback function instead of inline event handlers;
+            messageDialog.Commands.Add(new UICommand(
+                "Okay"));
+
+            // Set the command that will be invoked by default
+            messageDialog.DefaultCommandIndex = 0;
+
+            // Set the command to be invoked when escape is pressed
+            messageDialog.CancelCommandIndex = 0;
+
+            // Show the message dialog
+            await messageDialog.ShowAsync();
+        }
+    }
+}
---
+++ App2/MainPage.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Windows.System;
+using System.Collections.Generic;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.Storage.Streams;
+using Windows.UI.Input.Inking;
+using Windows.UI.Popups;
+using System.Threading.Tasks;
+using System.Linq;
+using System.IO;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace DisplayInkApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        static int INK_SAMPLE_INDEX = 0;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (INK_SAMPLE_INDEX != InkSamples.INK_SAMPLE_ARRAY.Length - 1)
+                {
+                    INK_SAMPLE_INDEX += 1;
+                }
+                DisplayRecord(INK_SAMPLE_INDEX);
+            }
+            catch (Exception ex)
+            {
+                ErrorDisplay.ShowErrorMessage(ex.Message);
+            }
+        }
+
+        private async void DisplayRecord(int idx)
+        {
+            try
+            {
+                String InkIsf = InkSamples.INK_SAMPLE_ARRAY[idx];
+                InMemoryRandomAccessStream stream = new InMemoryRandomAccessStream();
+
+
+                using (DataWriter writer = new DataWriter(stream.GetOutputStreamAt(0)))
+                {
+                    writer.WriteBytes(HexStringToByteArray(InkIsf));
+                    await writer.StoreAsync();
+                    var inkStrokeContainer = new InkStrokeContainer();
+                }
+
+                try
+                {
+                    await InkCanvas.InkPresenter.StrokeContainer.LoadAsync(stream);
+
+                }
+                catch (Exception ex)
+                {
+                    ErrorDisplay.ShowErrorMessage(ex.Message);
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorDisplay.ShowErrorMessage(ex.Message);
+            }
+        }
+        
+            private static byte[] HexStringToByteArray(String hex)
+            {
+                int NumberBytes = (hex.Length - 2) / 2;
+                byte[] bytes = new byte[NumberBytes];
+
+                using (var sr = new StringReader(hex))
+                {
+                    // read first 2 bytes ("0x")
+                    for (int i = 0; i < 2; i++)
+                    {
+                        sr.Read();
+                    }
+
+                    // Read hex characters
+                    for (int i = 0; i < NumberBytes; i++)
+                    {
+                        bytes[i] =
+                            Convert.ToByte(new string(new char[2] { (char)sr.Read(), (char)sr.Read() }), 16);
+                    }
+                }
+
+                return bytes;
+            }
+
+    }
+}
